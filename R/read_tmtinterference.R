985a4ceda4bd58795817a8dfb402819a33a6096b
mgerault <67689583+mgerault@users.noreply.github.com>
2025-02-17 12:25:43 +0100

    Add files via upload

--- R/read_tmtinterference.R
+++ R/read_tmtinterference.R
@@ -194,5 +194,38 @@ read_tmtinterference <- function(f){
                         }
                       }))
 
+  TMTion <- list("10" = c("126", "127N", "127C", "128N", "128C", "129N", "129C", "130N", "130C", "131"),
+                 "11" = c("126", "127N", "127C", "128N", "128C", "129N", "129C", "130N", "130C", "131N", "131C"),
+                 "16" = c("126", "127N", "127C", "128N", "128C", "129N", "129C", "130N", "130C", "131N", "131C", "132N", "132C", "133N", "133C", "134N"),
+                 "18" = c("126", "127N", "127C", "128N", "128C", "129N", "129C", "130N", "130C", "131N", "131C", "132N", "132C", "133N", "133C", "134N", "134C", "135N"),
+                 "32" = c("126", "127N", "127C", "128N", "128C", "129N", "129C", "130N", "130C", "131N", "131C", "132N", "132C", "133N", "133C", "134N",
+                          "127D", "128ND", "128CD", "129ND", "129CD", "130ND", "130CD", "131ND", "131CD", "132ND", "132CD", "133ND", "133CD", "134ND", "134CD", "135ND"),
+                 "35" = c("126", "127N", "127C", "128N", "128C", "129N", "129C", "130N", "130C", "131N", "131C", "132N", "132C", "133N", "133C", "134N", "134C", "135N",
+                          "127D", "128ND", "128CD", "129ND", "129CD", "130ND", "130CD", "131ND", "131CD", "132ND", "132CD", "133ND", "133CD", "134ND", "134CD", "135ND", "135CD")
+                 )
+  if(any(!(df[[1]] %in% TMTion[[paste(nrow(df))]]))){
+    mtag_missing <- TMTion[[paste(nrow(df))]][which(!(TMTion[[paste(nrow(df))]] %in% df[[1]]))]
+    mtag_wrong <- df[[1]][which(!(df[[1]]) %in% TMTion[[paste(nrow(df))]])]
+    mtag_pb <- data.frame("mtag_missing" = mtag_missing, "mtag_wrong" = mtag_wrong)
+
+    for(i in 1:nrow(mtag_pb)){
+      df[[1]][which(df[[1]] == mtag_pb$mtag_wrong)] <- mtag_pb$mtag_missing
+      if(any(grepl(paste0("\\(", mtag_pb$mtag_wrong, "\\)"), df[[2]]))){
+        df[[2]] <- gsub(paste0("\\(", mtag_pb$mtag_wrong, "\\)"),
+                        paste0("(", mtag_pb$mtag_missing, ")"),
+                        df[[2]])
+      }
+      if(any(grepl(paste0("\\(", mtag_pb$mtag_wrong, "\\)"), df[[3]]))){
+        df[[3]] <- gsub(paste0("\\(", mtag_pb$mtag_wrong, "\\)"),
+                        paste0("(", mtag_pb$mtag_missing, ")"),
+                        df[[3]])
+      }
+
+      message(paste("The Mass.Tag mislabelled as", mtag_pb$mtag_wrong,
+                    "in your data has been named", mtag_pb$mtag_missing,
+                    "by default."))
+    }
+  }
+
   return(df)
 }
